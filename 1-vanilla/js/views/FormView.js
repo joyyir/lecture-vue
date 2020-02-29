d9dc86f52e95f23075c6467c7c9e45f99fb57636
Jang Jun-yeong <joyyir@naver.com>
2020-03-01 00:42:54 +0900

    인프런 강의 "검색폼 구현 4 (구현)" 까지 학습

---
+++ 1-vanilla/js/views/FormView.js
@@ -0,0 +1,40 @@
+import View from './View.js'
+
+const tag = '[FormView]'
+
+const FormView = Object.create(View)
+
+FormView.setup = function (el) {
+    this.init(el)
+    this.inputEl = el.querySelector('[type=text')
+    this.resetEl = el.querySelector('[type=reset')
+    this.showResetBtn(false)
+    this.bindEvents()
+    return this
+}
+
+FormView.showResetBtn = function(show = true) {
+    this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+FormView.bindEvents = function () {
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+    this.resetEl.addEventListener('click', e => this.onClickReset(e))
+}
+
+FormView.onKeyup = function (e) {
+    const enter = 13
+    const isNotEmpty = this.inputEl.value.length
+    this.showResetBtn(isNotEmpty)
+    if (!isNotEmpty) this.emit('@reset')
+    if (e.keyCode !== enter) return
+    this.emit('@submit', {input: this.inputEl.value})
+}
+
+FormView.onClickReset = function (e) {
+    this.emit('@reset')
+    this.showResetBtn(false)
+    this.inputEl.focus()
+}
+
+export default FormView
