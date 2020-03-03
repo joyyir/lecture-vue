754b088ba208abed575e09647ce5c43e013b51b4
Jang Jun-yeong <joyyir@naver.com>
2020-03-03 12:32:31 +0900

    인프런 강의 "FormComponent, ResultComponent 구현" 까지 학습

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -2,6 +2,9 @@ import SearchModel from './models/SearchModel.js'
 import KeywordModel from './models/KeywordModel.js'
 import HistoryModel from './models/HistoryModel.js'
 
+import FormComponent from './components/FormComponent.js'
+import ResultComponent from './components/ResultComponent.js'
+
 new Vue({
   el: '#app',
   data: {
@@ -13,17 +16,19 @@ new Vue({
     history: [],
     searchResult: []
   },
+  components: {
+    'search-form': FormComponent,
+    'search-result': ResultComponent
+  },
   created() {
     this.selectedTab = this.tabs[0]
     this.fetchKeyword()
     this.fetchHistory()
   },
   methods: {
-    onSubmit(e) {
+    onSubmit(query) {
+      this.query = query
       this.search()
-    },
-    onKeyup(e) {
-      if (!this.query.length) this.resetForm()
     },
     onReset(e) {
       this.resetForm()
